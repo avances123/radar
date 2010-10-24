bbf3295d483bfd5df9e3803385e6e36a5e14352f
avances123 <avances123@gmail.com>
2010-10-24 06:15:54 +0200

    added tests

--- src/scraper/radar_retriever.py
+++ src/scraper/radar_retriever.py
@@ -2,37 +2,49 @@
 from datetime import datetime,timedelta
 import urllib2
 import os
+from urllib2 import URLError
 
 
-def roundMinutes():
+def getTimeStamp(delay=30):
     # Redondeo del minuto
     timestamp = datetime.utcnow()
     m = timestamp.minute
     m = m / 10 * 10
     timestamp = timestamp.replace(minute=m)
-    d = timedelta(minutes=30)
+    d = timedelta(minutes=delay)
     timestamp = timestamp - d
-    return timestamp
+    return timestamp.strftime("%Y%m%d%H%M")
 
+def getImage(url):
+    try:
+        imgData = urllib2.urlopen(url).read()
+    except URLError:
+        raise
+    return imgData
 
+def saveImage(stream,filename,folder):
+    try:
+        output = open(os.path.join(folder, filename), 'wb')
+        output.write(stream)
+        output.close()
+    except:
+        pass
+    
 
 def main():
     _regions = ['co', 'sa', 'ss', 'vd', 'za', 'ba', 'ma', 'cc', 'va', 'mu', 'se', 'ml', 'am', 'pm', 'ca']
     _url = 'http://www.aemet.es/imagenes_d/eltiempo/observacion/radar/'
     _dest = '/home/fabio/radar/img'
-    timestamp = roundMinutes()
 # Creacion del timestamp en la url
     for i in _regions:
         # Creamos la url para la region
-        filename = timestamp.strftime("%Y%m%d%H%M") + '_r8' + i + '.gif'
+        filename = getTimeStamp() + '_r8' + i + '.gif'
         url = _url + filename
         try:
-            imgData = urllib2.urlopen(url).read()
-            output = open(os.path.join(_dest, filename), 'wb')
-            output.write(imgData)
-            output.close()
+            imgData = getImage(url)
+            saveImage(imgData, filename, _dest)
             print str(datetime.now()) + " Exito bajando " + url
-        except:
+        except Exception:
             print str(datetime.now()) + " Error bajando " + url
 
 if __name__ == '__main__':
