a6a84c89dfec4ca216dfe4492c1411c46a835989
avances123 <avances123@gmail.com>
2010-10-25 04:02:15 +0200

    getting code from gdal_merge.py

--- src/radar/main.py
+++ src/radar/main.py
@@ -12,8 +12,11 @@ from scraper import retriever
 if __name__ == '__main__':
 
     ret = retriever.Retriever()
-    list_image = ret.getListOfImages(['ma'])
+    list_image = ret.getListOfImages(['ss'])
     for i in list_image:
         radar = compositor.RadarRegional(i)
-        #radar.report()
+        radar.report()
         radar.dumpToGeoTiff('out.tif')
+        for i in range(min(256,radar.ct.GetCount())):
+            entry = radar.ct.GetColorEntry(i)
+            print entry
