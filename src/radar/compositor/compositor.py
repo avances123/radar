54a19221ab8f740bd115bd72bea297ccdff5c15c
avances123 <avances123@gmail.com>
2010-10-25 02:21:21 +0200

    dump a regional raster into a geotiff

--- src/radar/compositor/compositor.py
+++ src/radar/compositor/compositor.py
@@ -4,10 +4,9 @@ Created on 24/10/2010
 @author: fabio
 '''
 
-from osgeo import gdal, gdal_array
-gdal.TermProgress = gdal.TermProgress_nocb
-from osgeo.gdalconst import GA_ReadOnly
-import numpy
+from osgeo import gdal
+#gdal.TermProgress = gdal.TermProgress_nocb
+import sys
 
 
 
@@ -15,84 +14,61 @@ class RadarRegional(object):
     """
      # PyUML: Do not remove this line! # XMI_ID:_H49OgN-ZEd-4cbf1aHA2Wg
     """
-
-    # Image Property
-    def get_imagepath(self):
-        return self.__imagepath
-    def set_imagepath(self, value):
-        self.__imagepath = value
-    def del_imagepath(self):
-        del self.__imagepath
-    def get_rows(self):
-        return self.__rows
-    def get_cols(self):
-        return self.__cols
-    def set_rows(self, value):
-        self.__rows = value
-    def set_cols(self, value):
-        self.__cols = value
-    def del_rows(self):
-        del self.__rows
-    def del_cols(self):
-        del self.__cols
-    image = property(get_imagepath, set_imagepath, del_imagepath, "Objeto imagen de este radar")
-    rows = property(get_rows, set_rows, del_rows, "Number of rows of this dataset")
-    cols = property(get_cols, set_cols, del_cols, "Number of cols of this dataset")
-    
-
-
-
-    
     # Constructor
-    def __init__(self, imagepath):
-        self.__imagepath = imagepath
-        self.dataset = self.getDataSet(imagepath)
-        self.rows = self.dataset.RasterYSize
-        self.cols = self.dataset.RasterXSize
-        self.bands = self.dataset.RasterCount
-        
-        
-    
-    # TODO Poner mas control
-    def getDataSet(self,path):
-        gdal.AllRegister()
-        try:
-            ds = gdal.Open(path, GA_ReadOnly)
-            if ds is None:
-                raise Exception
-        except:
-            print 'Error obteniendo dataset en la imagen: ' + path
-        return ds
-    
+    def __init__(self, filename):
+        """
+        Initialize file_info from filename
 
-    def getDataAsArray(self,path):
-        try:
-            dataset = self.getDataSet(path)
-            band = dataset.GetRasterBand(1)
-            #scanline = inband.ReadAsArray(0, i, inband.XSize, 1, inband.XSize, 1)
-            #def ReadAsArray(self, xoff=0, yoff=0, win_xsize=None, win_ysize=None,buf_xsize=None, buf_ysize=None, buf_obj=None):
-            data = band.ReadAsArray(0, 0, band.XSize, band.YSize, band.XSize, band.YSize)
-        except:
-            pass
-        return data
-    
-    # No la uso
-    def getHistogram(self,path):
-        try:
-            dataset = self.getDataSet(path)
-            band = dataset.GetRasterBand(1)
-            hist = band.GetDefaultHistogram(force = 0)
-            if hist is None:
-                print('No default histogram.')
-            else:
-                print('Default Histogram:')
-                print('Min: ', hist[0])
-                print('Max: ', hist[1])
-                print('Buckets: ', hist[2])
-                print('Histogram: ', hist[3])
+        filename -- Name of file to read.
 
-        except:
-            pass
+        Returns 1 on success or 0 if the file can't be opened.
+        """
+        fh = gdal.Open(filename)
+        if fh is None:
+            print('Unable to open %s' % filename)
+            sys.exit(1)
+
+        self.dataset = fh
+        self.filename = filename
+        self.bands = fh.RasterCount
+        self.xsize = fh.RasterXSize
+        self.ysize = fh.RasterYSize
+        self.band_type = fh.GetRasterBand(1).DataType
+        self.projection = fh.GetProjection()
+        self.geotransform = fh.GetGeoTransform()
+        self.ulx = self.geotransform[0]
+        self.uly = self.geotransform[3]
+        self.lrx = self.ulx + self.geotransform[1] * self.xsize
+        self.lry = self.uly + self.geotransform[5] * self.ysize
+
+        ct = fh.GetRasterBand(1).GetRasterColorTable()
+        if ct is not None:
+            self.ct = ct.Clone()
+        else:
+            self.ct = None
+
+    def report( self ):
+        print('Filename: '+ self.filename)
+        print('File Size: %dx%dx%d' \
+              % (self.xsize, self.ysize, self.bands))
+        print('Pixel Size: %f x %f' \
+              % (self.geotransform[1],self.geotransform[5]))
+        print('UL:(%f,%f)   LR:(%f,%f)' \
+              % (self.ulx,self.uly,self.lrx,self.lry))
+        
+    def dumpToGeoTiff(self,newfile):
+        def progress_cb( complete, message, cb_data ):
+            print('%s %d' % (cb_data, complete))
+        geotiff = gdal.GetDriverByName("GTiff")
+        if geotiff is None:
+            print('GeoTIFF driver not registered.')
+            sys.exit(1)
+        print('Importing to Tiled GeoTIFF file: %s' % newfile)
+        new_dataset = geotiff.CreateCopy( newfile, self.dataset, 0,
+                                  ['TILED=YES',],
+                                  callback = progress_cb,
+                                  callback_data = '' )
+
 
 if __name__ == '__main__':
     pass
--- src/radar/main.py
+++ src/radar/main.py
@@ -5,17 +5,15 @@ Created on 24/10/2010
 '''
 from compositor import compositor
 from scraper import retriever
-import numpy
+
+
+
 
 if __name__ == '__main__':
 
     ret = retriever.Retriever()
     list_image = ret.getListOfImages(['ma'])
-    list_radars = []
     for i in list_image:
         radar = compositor.RadarRegional(i)
-        data = radar.getDataAsArray(i)
-        print 'Tamanio de la banda: ' + str(data.shape)
-        print data
-        radar.getHistogram('/home/fabio/meteologica/tiffs/predicciones500-alarmaicona-20100513-d0.tiff')
-        list_radars.append(radar)
+        #radar.report()
+        radar.dumpToGeoTiff('out.tif')
