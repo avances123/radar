433516c9e20ffaf1f4c5e0581173c531ff9e66de
avances123 <avances123@gmail.com>
2010-10-25 23:00:35 +0200

    Modelling Retriever1

--- umbrello/code/Radar/Retriever.py
+++ umbrello/code/Radar/Retriever.py
@@ -8,11 +8,42 @@ class Retriever(object):
   :author:
   """
 
-  def downloadImages(self, regions = None):
+  """ ATTRIBUTES
+
+   
+
+  REGIONS  (public)
+
+  """
+
+  REGIONS = ['co', 'sa', 'ss', 'vd', 'za', 'ba', 'ma', 'cc', 'va', 'mu', 'se', 'ml', 'am', 'pm', 'ca']
+
+
+  def downloadImages(self, regions = REGIONS):
     """
      
 
     @param array regions : List of regions we want to download
+    @return  :
+    @author
+    """
+    pass
+
+  def referenceImages(self, regions = REGIONS):
+    """
+     
+
+    @param array regions : 
+    @return  :
+    @author
+    """
+    pass
+
+  def updateRegions(self, regions = REGIONS):
+    """
+     
+
+    @param array regions : 
     @return  :
     @author
     """
